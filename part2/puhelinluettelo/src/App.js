edcb01fd5ca720a1a000eb8fcfcd5afe1596e0f1
Lauri Kyttälä <laurikyttala@Lauri-Air.home>
2021-10-21 15:42:02 +0300

    added part 2 until exercise 2.6

---
+++ part2/puhelinluettelo/src/App.js
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+
+const App = () => {
+  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [newName, setNewName] = useState("");
+
+  const addPerson = (event) => {
+    event.preventDefault();
+    setPersons(persons.concat({ name: newName }));
+    // setPersons((persons) => [...persons, { name: newName }]);
+  };
+
+  const handlePersonChange = (event) => {
+    setNewName(event.target.value);
+  };
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          name: <input value={newName} onChange={handlePersonChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map((person) => (
+        <li key={person.name}>{person.name}</li>
+      ))}
+    </div>
+  );
+};
+
+export default App;
---
+++ part2/puhelinluettelo/src/components/Person.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Person({person}) {
+    return (
+        <div>
+            <li>{person.content}</li>           
+        </div>
+    )
+}
