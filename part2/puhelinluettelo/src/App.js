0f1eb04f8c72f2a9cf3ed63940267301aced432e
Lauri Kyttälä <laurikyttala@Lauri-Air.home>
2021-10-22 17:11:03 +0300

    exer 2.10 step 1 - const Filter

--- part2/puhelinluettelo/src/App.js
+++ part2/puhelinluettelo/src/App.js
@@ -1,6 +1,31 @@
 import React, { useState } from "react";
 import "./App.css";
 
+const Filter = ({ setNewFilter }) => {
+  return (
+    <div>
+      <label>filter:</label>
+      <input onChange={(event) => setNewFilter(event.target.value)} />
+    </div>
+  );
+};
+
+// const PersonForm = ({setNewPerson, newPerson, addPerson}) => {
+//   return (
+//     <div>
+//           <div>
+//             <h2>Add a new</h2>
+//             <label>name:</label>
+//             <input onChange={(event) => setNewPerson({...newPerson, name: event.target.value})}/>
+//           </div>
+//           <div>
+//             <label>number:</label>
+//             <input onChange={(event) => setNewPerson({...newPerson, number: event.target.value})} />
+//           </div>
+//     </div>
+//   )
+// };
+
 const App = () => {
   const [persons, setPersons] = useState([
     { name: "Arto Hellas", number: "040-123456" },
@@ -8,18 +33,16 @@ const App = () => {
     { name: "Dan Abramov", number: "12-43-234345" },
     { name: "Mary Poppendieck", number: "39-23-6423122" },
   ]);
-  const [newName, setNewName] = useState("");
-  const [newNumber, setNewNumber] = useState("");
+  const [newPerson, setNewPerson] = useState({ name: "", number: "" });
   const [newFilter, setNewFilter] = useState("");
 
   const addPerson = (event) => {
     event.preventDefault();
     const names = persons.map((person) => person.name);
-    if (names.includes(newName)) {
-      alert(`${newName} is already added to the phonebook`);
+    if (names.includes(newPerson.name)) {
+      alert(`${newPerson.name} is already added to the phonebook`);
     } else {
-      setPersons(persons.concat({ name: newName, number: newNumber }));
-      // setPersons((persons) => [...persons, { name: newName, number: newNumber }]);
+      setPersons((persons) => [...persons, newPerson]);
     }
   };
 
@@ -29,16 +52,20 @@ const App = () => {
       <form onSubmit={addPerson}>
         <div>
           <div>
-            <label>filter:</label>
-            <input onChange={(event) => setNewFilter(event.target.value)} />
+            <Filter setNewFilter={setNewFilter} />
           </div>
-          <h2>Add a new</h2>
-          <label>name:</label>
-          <input onChange={(event) => setNewName(event.target.value)} />
-        </div>
-        <div>
-          <label>number:</label> 
-          <input onChange={(event) => setNewNumber(event.target.value)} />
+          <>
+          {/* <PersonForm setNewPerson={setNewPerson} newPerson={newPerson} addPerson={addPerson} /> */}
+          <div>
+            <h2>Add a new</h2>
+            <label>name:</label>
+            <input onChange={(event) => setNewPerson({...newPerson, name: event.target.value})}/>
+          </div>
+          <div>
+            <label>number:</label>
+            <input onChange={(event) => setNewPerson({...newPerson, number: event.target.value})} />
+          </div>
+          </>
         </div>
         <div>
           <button type="submit">add</button>
