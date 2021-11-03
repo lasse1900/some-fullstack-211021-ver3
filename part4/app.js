7103311a48822ed9227f644611f337cfe2110d85
Lauri Kyttälä <laurikyttala@Lauri-Air.home>
2021-11-03 16:02:32 +0200

    4.2b added logger middleware

--- part4/app.js
+++ part4/app.js
@@ -3,11 +3,11 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const blogsRouter = require('./controllers/blogs')
-// const middleware = require('./utils/middleware')
-// const logger = require('./utils/logger')
+const middleware = require('./utils/middleware')
+const logger = require('./utils/logger')
 const mongoose = require('mongoose')
 
-// logger.info('connecting to', config.MONGODB_URI)
+logger.info('connecting to', config.MONGODB_URI)
 
 mongoose.connect(config.MONGODB_URI)
   .then(() => {
@@ -20,11 +20,11 @@ mongoose.connect(config.MONGODB_URI)
 app.use(cors())
 app.use(express.static('build'))
 app.use(express.json())
-// app.use(middleware.requestLogger)
+app.use(middleware.requestLogger)
 
 app.use('/api/blogs', blogsRouter)
 
-// app.use(middleware.unknownEndpoint)
-// app.use(middleware.errorHandler)
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
 module.exports = app
