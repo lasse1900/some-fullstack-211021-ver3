c7be45e075f16ac546b4693cccc8e66f48632007
Lauri Kyttälä <laurikyttala@Lauri-Air.home>
2021-11-03 19:21:13 +0200

    4.5 favourite Blog step3

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -1,17 +1,39 @@
 // eslint-disable-next-line no-unused-vars
-const dummy = blogs => {
+const dummy = (blogs) => {
   return 1
 }
 
-const totalLikes = blogs => {
+const totalLikes = (blogs) => {
   const reducer = (sum, blog) => {
     return sum + blog.likes
   }
 
-  return blogs.length === 0 ? 0 : blogs.reduce(reducer, 0)
+  return blogs.length === 0 ? 0 : blogs.reduce(reducer, 0);
+}
+
+const favoriteBlog = (blogs) => {
+  const reducer = (acc, blog) => {
+    if (blog.likes > acc.likes) {
+      return blog.likes
+    }
+    return acc
+  }
+  return blogs.length === 0 ? null : blogs.reduce(reducer, blogs[0])
+}
+
+const favoriteWriter = (blogs) => {
+  const reducer = (acc, blog) => {
+    if (blog.likes > acc.likes) {
+      return { title: blog.title, author: blog.author, likes: blog.likes }
+    }
+    return acc
+  }
+  return blogs.length === 0 ? null : blogs.reduce(reducer, blogs[0])
 }
 
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
+  favoriteWriter
 }
