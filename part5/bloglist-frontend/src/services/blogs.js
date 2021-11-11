82d5e35783350fab06515480bbad60f78eec8dde
Lauri Kyttälä <laurikyttala@Lauri-Air.home>
2021-11-11 15:04:25 +0200

    5.3 logged user can create new blogs step3

--- part5/bloglist-frontend/src/services/blogs.js
+++ part5/bloglist-frontend/src/services/blogs.js
@@ -7,12 +7,12 @@ const setToken = newToken => {
   token = `bearer ${newToken}`
 }
 
-const create = async newObject => {
+const create = async newBlog => {
   const config = {
-    headers: { Authorization: token },
+    headers: { Authorization: token }
   }
 
-  const response = await axios.post(baseUrl, newObject, config) 
+  const response = await axios.post(baseUrl, newBlog, config)
   return response.data
 }
 
@@ -21,10 +21,5 @@ const getAll = () => {
   return request.then(response => response.data)
 }
 
-const update = (id, newObject) => {
-  const request = axios.put(`${ baseUrl } /${id}`, newObject)
-  return request.then(response => response.data)
-}
-
 // eslint-disable-next-line import/no-anonymous-default-export
-export default { getAll, create, update, setToken }
+export default { getAll, create, setToken }
