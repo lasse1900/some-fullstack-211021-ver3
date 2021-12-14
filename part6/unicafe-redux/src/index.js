a69ac92d3e6c727bde54d27fb0c9875cfd6d2e84
Lauri Kyttälä <laurikyttala@Lauri-Air.home>
2021-12-14 17:34:47 +0200

    part 6 exercises 6.1 & 6.2 Unicafe with redux

---
+++ part6/unicafe-redux/src/index.js
@@ -0,0 +1,46 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { createStore } from "redux";
+import reducer from "./reducer";
+import "./style.css";
+
+const store = createStore(reducer);
+
+const App = () => {
+  const good = () => {
+    store.dispatch({ type: "GOOD" });
+  };
+  const ok = () => {
+    store.dispatch({ type: "OK" });
+  };
+  const bad = () => {
+    store.dispatch({ type: "BAD" });
+  };
+  const reset = () => {
+    store.dispatch({ type: "ZERO" });
+  };
+
+  return (
+    <>
+      <div className="header">Unicafe</div>
+      <div className="display-buttons">
+        <button onClick={good}>good</button>
+        <button onClick={ok}>ok</button>
+        <button onClick={bad}>bad</button>
+        <button onClick={reset}>reset</button>
+      </div>
+      <div className="display-counts">
+        <div>hyvä {store.getState().good}</div>
+        <div>neutraali {store.getState().ok}</div>
+        <div>huono {store.getState().bad}</div>
+      </div>
+    </>
+  );
+};
+
+const renderApp = () => {
+  ReactDOM.render(<App />, document.getElementById("root"));
+};
+
+renderApp();
+store.subscribe(renderApp);
