6846a17947248a6716d0166aeda11a1434f78826
Lauri Kyttälä <laurikyttala@Lauri-Air.home>
2021-12-17 17:43:38 +0200

    6.9 sparate store created

--- part6/redux-anecdotes/src/index.js
+++ part6/redux-anecdotes/src/index.js
@@ -1,10 +1,7 @@
 import ReactDOM from "react-dom";
-import { createStore } from "redux";
 import App from "./App";
-import reducer from "./reducers/anecdoteReducer";
 import { Provider } from "react-redux";
-
-const store = createStore(reducer);
+import { store } from "./store";
 
 ReactDOM.render(
   <Provider store={store}>
