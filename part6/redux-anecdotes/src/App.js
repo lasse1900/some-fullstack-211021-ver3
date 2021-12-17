2783fd0b9378fa34a6c1ed443138eb154ef0c8c8
Lauri Kyttälä <laurikyttala@Lauri-MacBook-Air.local>
2021-12-17 14:27:31 +0200

    6.5 sort anecdotes by votes

--- part6/redux-anecdotes/src/App.js
+++ part6/redux-anecdotes/src/App.js
@@ -24,15 +24,17 @@ const App = (props) => {
     <div className="layout">
       <h2>Anecdotes</h2>
       <div className="anecdote-list">
-        {anecdotes.map((anecdote) => (
-          <div key={anecdote.id}>
-            <div>{anecdote.content}</div>
-            <div className="vote-count">
-              has {anecdote.votes}
-              <button onClick={() => vote(anecdote.id)}>vote</button>
+        {anecdotes
+          .sort((a, b) => b.votes - a.votes)
+          .map((anecdote) => (
+            <div key={anecdote.id}>
+              <div>{anecdote.content}</div>
+              <div className="vote-count">
+                has {anecdote.votes}
+                <button onClick={() => vote(anecdote.id)}>vote</button>
+              </div>
             </div>
-          </div>
-        ))}
+          ))}
       </div>
 
       <h2>create new</h2>
