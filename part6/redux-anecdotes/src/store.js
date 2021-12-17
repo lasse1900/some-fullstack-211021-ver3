1a9c4f39591daac479dd93183c35bca143c2bdfa
Lauri Kyttälä <laurikyttala@Lauri-Air.home>
2021-12-17 17:50:32 +0200

    corredted export const store

--- part6/redux-anecdotes/src/store.js
+++ part6/redux-anecdotes/src/store.js
@@ -2,6 +2,4 @@ import { createStore } from "redux";
 import reducer from "./reducers/anecdoteReducer";
 import { composeWithDevTools } from "redux-devtools-extension";
 
-const store = createStore(reducer, composeWithDevTools());
-
-export default store;
+export const store = createStore(reducer, composeWithDevTools());
